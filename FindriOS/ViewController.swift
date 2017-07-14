a5be971e67f6177a43d78236f709f9256b9313f0
EvilAnnie <annie240897@gmail.com>
2017-07-13 22:04:51 -0400

    Initial Commit

---
+++ FindriOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindriOS
+//
+//  Created by Ananya Gupta on 7/13/17.
+//  Copyright (c) 2017 Ananya Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
