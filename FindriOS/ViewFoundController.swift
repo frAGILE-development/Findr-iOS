70090e2fd93ddcd48ff45487b9700298e95c0acc
EvilAnnie <annie240897@gmail.com>
2017-07-17 17:52:11 -0400

    ui pages for items

---
+++ FindriOS/ViewFoundController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewFoundController.swift
+//  FindriOS
+//
+//  Created by Ananya Gupta on 7/17/17.
+//  Copyright (c) 2017 Ananya Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewFoundController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClickBack(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
