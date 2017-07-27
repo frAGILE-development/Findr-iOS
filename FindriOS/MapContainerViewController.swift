e043287b82653069eb17fb8d04210fc32842aee6
Ananya240 <annie240897@gmail.com>
2017-07-26 21:01:48 -0400

    Plotting lost and found item locations

--- FindriOS/MapContainerViewController.swift
+++ FindriOS/MapContainerViewController.swift
@@ -7,41 +7,79 @@
 //
 
 import UIKit
+import CoreLocation
 
-class MapContainerViewController: UIViewController {
+class MapContainerViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
     }
     
+    var data = [FoundItem]()
+    var data2 = [LostItem]()
+    
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()    }
+    
+    var filePath: String {
+        let manager = NSFileManager.defaultManager();
+        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).last
+        
+        var notString =  url!.URLByAppendingPathComponent("Data")
+        var mod = notString.absoluteString!
+        var ret = mod.stringByReplacingOccurrencesOfString("file://", withString: "", options: .allZeros, range: nil)
+        return ret;
+    }
+    
+    var filePath2: String {
+        let manager = NSFileManager.defaultManager();
+        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first
+        
+        var notString =  url!.URLByAppendingPathComponent("Data2")
+        var mod = notString.absoluteString!
+        var ret = mod.stringByReplacingOccurrencesOfString("file://", withString: "", options: .allZeros, range: nil)
+        return ret;
+    }
+    
+    private func loadData() {
+        if let ourData = NSKeyedUnarchiver.unarchiveObjectWithFile(filePath) as? [FoundItem] {
+            data = ourData
+        }
+        
+        if let ourData2 = NSKeyedUnarchiver.unarchiveObjectWithFile(filePath2) as? [LostItem] {
+            data2 = ourData2
+        }
+        
     }
     
     override func loadView() {
-        let camera = GMSCameraPosition.cameraWithLatitude(33.777427, longitude: -84.397609, zoom: 12.0)
+        loadData()
+        let camera = GMSCameraPosition.cameraWithLatitude(33.777427, longitude: -84.397609, zoom: 4.0)
         let mapView = GMSMapView.mapWithFrame(CGRect.nullRect, camera: camera)
         view = mapView
         
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 33.777427, longitude: -84.397609)
-        marker.title = "Atlanta"
-        marker.snippet = "GA"
-        marker.map = mapView
+        for item in data {
+            var lat = (item.Lat as! NSString).doubleValue
+            var long = (item.Long as! NSString).doubleValue
+            
+            var marker = GMSMarker()
+            marker.position = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            marker.title = "\(item.Item.uppercaseString)" + "  \(item.Date)" + "  \(item.Description)"
+            marker.snippet = "\(item.Address)" + "   Found Item"
+            marker.map = mapView
+        }
+        
+        for item in data2 {
+            var lat = (item.Lat as! NSString).doubleValue
+            var long = (item.Long as! NSString).doubleValue
+            
+            var marker = GMSMarker()
+            marker.position = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            marker.title = "\(item.Item.uppercaseString)" + "  \(item.Date)" + "  \(item.Description)"
+            marker.snippet = "\(item.Address)" + "   Lost Item"
+            marker.map = mapView
+        }
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
